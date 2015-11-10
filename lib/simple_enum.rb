f4efad0dd80e620db8800dbd968fb066a7d14436
necojackarc <necojackarc@gmail.com>
2015-11-11 01:16:37 +0900

    Add basic features

--- lib/simple_enum.rb
+++ lib/simple_enum.rb
@@ -1,5 +1,47 @@
+require "active_support"
+require "active_support/core_ext"
 require "simple_enum/version"
 
 module SimpleEnum
-  # Your code goes here...
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def simple_enum(single_size_hash)
+      fail ArgumentError if single_size_hash.size != 1
+
+      name, enum_values = single_size_hash.first
+
+      define_read_enum_values_method_to_class!(name.to_s.pluralize, enum_values)
+
+      enum_values.each do |key, _|
+        define_query_enum_state_method_to_instance!(name, key)
+        define_change_enum_state_method_to_instance!(name, key)
+      end
+    end
+
+    private
+
+    def define_read_enum_values_method_to_class!(pluralized_name, enum_values)
+      instance_variable_set("@#{pluralized_name}", enum_values)
+      define_singleton_method pluralized_name do
+        instance_variable_get "@#{pluralized_name}"
+      end
+    end
+
+    def define_query_enum_state_method_to_instance!(enum_name, value_name)
+      define_method "#{value_name}?" do
+        current_value = instance_variable_get("@#{enum_name}")
+        current_value == self.class.public_send(enum_name.to_s.pluralize.to_sym)[value_name]
+      end
+    end
+
+    def define_change_enum_state_method_to_instance!(enum_name, value_name)
+      define_method "#{value_name}!" do
+        next_value = self.class.public_send(enum_name.to_s.pluralize.to_sym)[value_name]
+        instance_variable_set("@#{enum_name}", next_value)
+      end
+    end
+  end
 end
