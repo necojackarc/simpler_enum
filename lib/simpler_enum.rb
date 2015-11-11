0ecee4450178e21580f0d00cddffc1eba015053c
necojackarc <necojackarc@gmail.com>
2015-11-11 16:44:49 +0900

    Extract generator from main module

--- lib/simpler_enum.rb
+++ lib/simpler_enum.rb
@@ -1,6 +1,7 @@
 require "active_support"
 require "active_support/core_ext"
 require "simpler_enum/version"
+require "simpler_enum/generator"
 
 module SimplerEnum
   def self.included(base)
@@ -11,59 +12,8 @@ module SimplerEnum
     def simpler_enum(single_size_hash)
       fail ArgumentError if single_size_hash.size != 1
 
-      name, enum_values = single_size_hash.first
-
-      define_read_enum_values_method_to_class!(name.to_s.pluralize, enum_values)
-      define_read_enum_value_method_to_instance!(name)
-      define_write_enum_value_method_to_instance!(name)
-
-      enum_values.each do |key, _|
-        define_query_enum_state_method_to_instance!(name, key)
-        define_change_enum_state_method_to_instance!(name, key)
-      end
-    end
-
-    private
-
-    def define_read_enum_values_method_to_class!(pluralized_name, enum_values)
-      instance_variable_set("@#{pluralized_name}", enum_values)
-      define_singleton_method pluralized_name do
-        instance_variable_get "@#{pluralized_name}"
-      end
-    end
-
-    def define_query_enum_state_method_to_instance!(enum_name, value_name)
-      define_method "#{value_name}?" do
-        current_value = instance_variable_get("@#{enum_name}")
-        current_value == self.class.public_send(enum_name.to_s.pluralize.to_sym)[value_name]
-      end
-    end
-
-    def define_change_enum_state_method_to_instance!(enum_name, value_name)
-      define_method "#{value_name}!" do
-        next_value = self.class.public_send(enum_name.to_s.pluralize.to_sym)[value_name]
-        instance_variable_set("@#{enum_name}", next_value)
-        public_send(enum_name)
-      end
-    end
-
-    def define_read_enum_value_method_to_instance!(enum_name)
-      define_method "#{enum_name}" do
-        value = instance_variable_get("@#{enum_name}")
-        self.class.public_send(enum_name.to_s.pluralize.to_sym).key(value)
-      end
-    end
-
-    def define_write_enum_value_method_to_instance!(enum_name)
-      define_method "#{enum_name}=" do |value|
-        next_value =
-          if value.is_a?(Symbol)
-            self.class.public_send(enum_name.to_s.pluralize.to_sym)[value]
-          else
-            value
-          end
-        instance_variable_set("@#{enum_name}", next_value)
-      end
+      enum_name, enum_values = single_size_hash.first
+      SimplerEnum::Generator.new(self, enum_name, enum_values).execute!
     end
   end
 end
---
+++ lib/simpler_enum/generator.rb
@@ -0,0 +1,73 @@
+module SimplerEnum
+  class Generator
+    def initialize(klass, enum_name, enum_values)
+      @klass, @enum_name, @enum_values = klass, enum_name, enum_values
+    end
+
+    def execute!
+      enum_name, enum_values = @enum_name, @enum_values
+
+      define_read_enum_values_method_to_class!(enum_name.to_s.pluralize, enum_values)
+      define_read_enum_value_method_to_instance!(enum_name)
+      define_write_enum_value_method_to_instance!(enum_name)
+
+      enum_values.each do |key, _|
+        define_query_enum_state_method_to_instance!(enum_name, key)
+        define_change_enum_state_method_to_instance!(enum_name, key)
+      end
+    end
+
+    private
+
+    def define_read_enum_values_method_to_class!(pluralized_name, enum_values)
+      @klass.class_eval do
+        instance_variable_set("@#{pluralized_name}", enum_values)
+        define_singleton_method pluralized_name do
+          instance_variable_get "@#{pluralized_name}"
+        end
+      end
+    end
+
+    def define_query_enum_state_method_to_instance!(enum_name, value_name)
+      @klass.class_eval do
+        define_method "#{value_name}?" do
+          current_value = instance_variable_get("@#{enum_name}")
+          current_value == self.class.public_send(enum_name.to_s.pluralize.to_sym)[value_name]
+        end
+      end
+    end
+
+    def define_change_enum_state_method_to_instance!(enum_name, value_name)
+      @klass.class_eval do
+        define_method "#{value_name}!" do
+          next_value = self.class.public_send(enum_name.to_s.pluralize.to_sym)[value_name]
+          instance_variable_set("@#{enum_name}", next_value)
+          public_send(enum_name)
+        end
+      end
+    end
+
+    def define_read_enum_value_method_to_instance!(enum_name)
+      @klass.class_eval do
+        define_method "#{enum_name}" do
+          value = instance_variable_get("@#{enum_name}")
+          self.class.public_send(enum_name.to_s.pluralize.to_sym).key(value)
+        end
+      end
+    end
+
+    def define_write_enum_value_method_to_instance!(enum_name)
+      @klass.class_eval do
+        define_method "#{enum_name}=" do |value|
+          next_value =
+            if value.is_a?(Symbol)
+              self.class.public_send(enum_name.to_s.pluralize.to_sym)[value]
+            else
+              value
+            end
+          instance_variable_set("@#{enum_name}", next_value)
+        end
+      end
+    end
+  end
+end
